4500c58193ce1f2b1d690963731d66e0ee2c89b4
Julius Naperkowski <jnaperkowski@sourcepark.de>
2015-11-13 18:19:57 +0100

    added white border around qr code to make it readable for more qr reader apps

--- src/qr.rs
+++ src/qr.rs
@@ -9,23 +9,36 @@ use image::{
     Luma
 };
 
+
 const SCALE_FACTOR: u32 = 10;
+const BORDER_BITS: u32 = 2;
 
 
 pub fn create(content: &str) -> Vec<u8> {
 	let code = QrCode::new(content.as_bytes()).unwrap();
 
-	let img_width = (code.width() as u32) * SCALE_FACTOR;
+	let unscaled_img_width = code.width() as u32 + BORDER_BITS;
+	let scaled_img_width = SCALE_FACTOR * unscaled_img_width;
+
+	let low_border = 0 as usize;
+	let high_border = (unscaled_img_width - 1) as usize;
 
 	// Construct a new by repeated calls to the supplied closure.
-	let buf = ImageBuffer::from_fn(img_width, img_width, |x, y| {
+	let buf = ImageBuffer::from_fn(scaled_img_width, scaled_img_width, |x, y| {
 		let x_bit = (x / SCALE_FACTOR) as usize;
 		let y_bit = (y / SCALE_FACTOR) as usize;
 
-		if code[(x_bit, y_bit)] {
-			Luma([0u8])
-		} else {
+		if x_bit == low_border 
+				|| y_bit == low_border
+				|| x_bit == high_border
+				|| y_bit == high_border {
 			Luma([255u8])
+		} else {
+			if code[(x_bit-1, y_bit-1)] {
+				Luma([0u8])
+			} else {
+				Luma([255u8])
+			}
 		}
 	});
 
