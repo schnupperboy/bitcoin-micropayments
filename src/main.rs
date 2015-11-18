72ba67c8b79fa18dba611731eded75650ea9d019
Julius Naperkowski <jnaperkowski@sourcepark.de>
2015-11-18 21:19:45 +0100

    print usage when number of cli args != 2

--- src/main.rs
+++ src/main.rs
@@ -14,12 +14,18 @@ pub mod server;
 pub mod qr;
 
 use std::env;
+use std::process;
 
 use hyper::server::Server;
 
 use server::{routes};
 
 fn main() {
+	if env::args().count() != 2 {
+		println!("Usage: cr-payment-bitcoin <ip-address> <port>");
+		process::exit(1);
+	}
+
 	let mut ip = env::args().nth(1).unwrap();
 	ip.push_str(":");
 	ip.push_str(&*env::args().nth(2).unwrap());
