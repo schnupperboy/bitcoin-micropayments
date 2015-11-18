9a26631be192c0c6b11f1b7b94556eb92bef0c82
Julius Naperkowski <jnaperkowski@sourcepark.de>
2015-11-18 21:12:02 +0100

    removed unused fields from blockchain.info response description of address subscription

--- src/blockchain_info.rs
+++ src/blockchain_info.rs
@@ -12,24 +12,15 @@ use websocket::result::WebSocketError;
 
 use payment_detection::{PaymentDetection, PaymentError};
 
+// For complete blockchain.info API see: https://blockchain.info/de/api/api_websocket
 
 #[derive(RustcDecodable)]
 pub struct AddressEvent {
-	pub op: String,
 	pub x: Transaction,
 }
 
 #[derive(RustcDecodable)]
 pub struct Transaction {
-	pub hash: String,
-	pub ver: u8,
-	pub vin_sz: u8,
-	pub vout_sz: u8,
-	pub lock_time: u64,
-	pub size: u64,
-	pub relayed_by: String,
-	pub tx_index: u64,
-	pub time: u64,
 	pub inputs: Vec<TransactionInput>,
 	pub out: Vec<TransactionOutput>
 }
@@ -37,19 +28,12 @@ pub struct Transaction {
 #[derive(RustcDecodable)]
 pub struct TransactionOutput {
 	pub value: u64,
-	//pub type_: u8, too lazy to write a custom decoder just because "type" appears to be a rust keyword
 	pub addr: String,
-	pub tx_index: u64,
-	pub spent: bool,
-	pub n: u32,
-	pub script: String,
 }
 
 #[derive(RustcDecodable)]
 pub struct TransactionInput {
 	pub prev_out: TransactionOutput,
-	pub sequence: u64,
-	pub script: String
 }
 
 #[derive(RustcEncodable)]
